6051af139e0c0cb01a436c64814dc30bcdf87ae2
Ksy0309 <swim040309@gmail.com>
2025-12-14 20:49:17 +0900

    B객체 생성, 점사이의 거리연산 함수 추가

--- HWGitProject/src/HWpackage/C.java
+++ HWGitProject/src/HWpackage/C.java
@@ -6,4 +6,11 @@ public class C {
 	public void printB(B b) {
 		System.out.println("x : " + b.x + ", y : " + b.y);
 	}
+	
+	public double getDis(B b1, B b2) {
+		double dx = b1.x - b2.x;
+		double dy = b1.y - b2.y;
+		double dis = Math.sqrt(dx * dx + dy * dy);
+		return dis;
+	}
 }
