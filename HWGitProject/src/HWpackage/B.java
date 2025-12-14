c2d8907914ad252600976e2064075aec2c612200
Ksy0309 <swim040309@gmail.com>
2025-12-14 20:37:33 +0900

    리포지토리 생성

---
+++ HWGitProject/src/HWpackage/B.java
@@ -0,0 +1,10 @@
+package HWpackage;
+
+public class B {
+	int x;
+	int y;
+	public B(int _x, int _y) {
+		x = _x;
+		y = _y;
+	}
+}
---
+++ HWGitProject/src/HWpackage/C.java
@@ -0,0 +1,9 @@
+package HWpackage;
+
+import HWpackage.B;
+
+public class C {
+	public void printB(B b) {
+		System.out.println("x : " + b.x + ", y : " + b.y);
+	}
+}
