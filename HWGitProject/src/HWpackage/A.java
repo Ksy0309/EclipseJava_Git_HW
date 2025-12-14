967d45cd6cb138c38ddda224793ce5478d7e4145
Ksy0309 <swim040309@gmail.com>
2025-12-14 21:18:53 +0900

    사각형의 높이, 두께 출력, 객체의 좌푯값 설정함수 추가

--- HWGitProject/src/HWpackage/A.java
+++ HWGitProject/src/HWpackage/A.java
@@ -5,6 +5,11 @@ import HWpackage.C;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!!");
-		
+		C c = new C();
+		B b1 = new B(0, 0);
+		B b2 = new B(0, 0);
+		b2.setX(2);
+		b2.setY(3);
+		System.out.println("rect height : " + c.getRectH(b1, b2) + ", rect weight : " + c.getRectW(b1, b2));
 	}
 }
