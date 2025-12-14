bde8242753c98d146110a7008c8cbe02c06fd54b
Ksy0309 <swim040309@gmail.com>
2025-12-14 21:06:10 +0900

    사각형 높이, 두께연산함수 추가

--- HWGitProject/src/HWpackage/C.java
+++ HWGitProject/src/HWpackage/C.java
@@ -6,4 +6,12 @@ public class C {
 	public void printB(B b) {
 		System.out.println("x : " + b.x + ", y : " + b.y);
 	}
+	
+	public int getRectH(B b1, B b2) {
+		return Math.abs(b1.y - b2.y);
+	}
+	
+	public int getRectW(B b1, B b2) {
+	return Math.abs(b1.x - b2.x);
+	}
 }
